5fca908e51a53535e20c55c567c0155a9219f544
Kazuyuki Fukaya <luizcarloskazuyukifukaya@gmail.com>
2023-09-29 19:29:53 +0900

    added GET OBJECT sample

---
+++ s3GetObject.py
@@ -0,0 +1,47 @@
+# AWS S3 boto3 reference
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/get_object_attributes.html#
+
+import boto3
+
+# Define the target bucket and prefix
+target_bucket = "kfukaya-tagging"
+target_file = "test.txt"
+
+print(target_bucket)
+print(target_file)
+
+session = boto3.Session(profile_name="wasabi")
+credentials = session.get_credentials()
+aws_access_key_id = credentials.access_key
+aws_secret_access_key = credentials.secret_key
+region = 'ap-northeast-1'
+endpoint_url = 'https://s3.' + region + '.wasabisys.com'
+
+print(region)
+print(endpoint_url)
+print(aws_access_key_id)
+print(aws_secret_access_key)
+
+s3 = boto3.client('s3',
+                  endpoint_url=endpoint_url,
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
+
+r = s3.get_object(
+    Bucket=target_bucket,
+    Key=target_file,
+)
+
+print(type(r))
+print(r)
+
+print('------------------')
+print(r['ResponseMetadata'])
+print('------------------')
+print(r['Metadata'])
+print('------------------')
+print(r['ETag'])
+print('------------------')
+print(r['VersionId'])
+print('------------------')
+print(r['ContentLanguage'])
