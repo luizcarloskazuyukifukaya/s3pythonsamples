3b22309adcd6500f7cf0bfe7cd504dfa500bfcfe
luizcarloskazuyukifukaya@gmail.com <luizcarloskazuyukifukaya@gmail.com>
2023-11-10 11:09:54 +0900

    Added PutObject sample code

---
+++ s3PutObject.py
@@ -0,0 +1,57 @@
+# AWS S3 boto3 reference
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/put_object.html
+
+import boto3
+
+# Define the target bucket and prefix
+target_bucket = "kfukaya-tagging"
+target_file = "data/sample_data.csv"
+
+print(target_bucket)
+print(target_file)
+
+session = boto3.Session(profile_name="wasabi")
+credentials = session.get_credentials()
+aws_access_key_id = credentials.access_key
+aws_secret_access_key = credentials.secret_key
+region = 'ap-northeast-1'
+endpoint_url = 'https://s3.' + region + '.wasabisys.com'
+
+print(region)
+print(endpoint_url)
+print(aws_access_key_id)
+print(aws_secret_access_key)
+
+s3 = boto3.client('s3',
+                  endpoint_url=endpoint_url,
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
+
+
+# upload an object (.data/sample_data.csv)
+r = s3.put_object(
+    Body=target_file,
+    Bucket=target_bucket,
+    Key=target_file,
+    ServerSideEncryption='AES256',
+    StorageClass='STANDARD_IA',
+)
+
+print(r)
+
+print(type(r))
+print(r)
+
+print('------------------')
+print('ResponseMetadata:')
+print(r['ResponseMetadata'])
+print('------------------')
+#print(r['Metadata'])
+#print('------------------')
+print('ETag:')
+print(r['ETag'])
+print('------------------')
+print('VersionId:')
+print(r['VersionId'])
+print('------------------')
+#print(r['ContentLanguage'])
--- s3ShowWasabiVersion.py
+++ s3ShowWasabiVersion.py
@@ -3,37 +3,58 @@
 
 import boto3
 
-# session = boto3.Session(profile_name="wasabi")
-session = boto3.Session(profile_name="wasabi")
-credentials = session.get_credentials()
-aws_access_key_id = credentials.access_key
-aws_secret_access_key = credentials.secret_key
-region = 'ap-northeast-1'
-endpoint_url = 'https://s3.' + region + '.wasabisys.com'
+def show_wasabi_system_details(region):
+    # session = boto3.Session(profile_name="wasabi")
+    session = boto3.Session(profile_name="wasabi")
+    credentials = session.get_credentials()
+    aws_access_key_id = credentials.access_key
+    aws_secret_access_key = credentials.secret_key
 
-print(region)
-print(endpoint_url)
-print(aws_access_key_id)
-print(aws_secret_access_key)
+    # default Wasabi US East 1 (N. Virginia)
+    endpoint_url = 'https://s3.' + region + '.wasabisys.com'
 
-#s3 = boto3.client('s3')
-s3 = boto3.client('s3',
-                  endpoint_url=endpoint_url,
-                  aws_access_key_id=aws_access_key_id,
-                  aws_secret_access_key=aws_secret_access_key)
+    print(region)
+    print(endpoint_url)
+    print(aws_access_key_id)
+    print(aws_secret_access_key)
 
-# Retrieve the list of existing buckets
-r = s3.list_buckets()
+    #s3 = boto3.client('s3')
+    s3 = boto3.client('s3',
+            region_name=region,
+            endpoint_url=endpoint_url,
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key)
 
-# Output the bucket names
-#print('Existing buckets:')
-#for bucket in r['Buckets']:
-#    print(f'  {bucket["Name"]}')
+    #print(s3)
+    # Retrieve the list of existing buckets
+    r = s3.list_buckets()
 
-#print(type(r))
-#print(r)
-#print(type(r['ResponseMetadata']))
-#print(r['ResponseMetadata'])
+    print('Wasabi Object Storage Server version: ' + (r['ResponseMetadata']['HTTPHeaders']['server']))
+    print('Current Date: ' + (r['ResponseMetadata']['HTTPHeaders']['date']))
+    
+    #sqs = boto3.resource('sqs')
+    #print(sqs)
+    #queue = sqs.Queue(url=endpoint_url)
+    #print(queue.url)
 
-print('Wasabi Object Storage Server version: ' + (r['ResponseMetadata']['HTTPHeaders']['server']))
-print('Current Date: ' + (r['ResponseMetadata']['HTTPHeaders']['date']))
+#-----------------------------------------------------
+#wasabi_regionsx = ['ap-northeast-1']
+ 
+wasabi_regions = ['us-east-1', 
+                  'us-east-2', 
+                  'us-central-1', 
+                  'us-west-1', 
+                  'ca-central-1', 
+                  'eu-central-1', 
+                  'eu-central-2', 
+                  'eu-west-1', 
+                  'eu-west-2', 
+                  'ap-northeast-1', 
+                  'ap-northeast-2', 
+                  'ap-southeast-1', 
+                  'ap-southeast-2']
+
+for target_region in wasabi_regions:
+    show_wasabi_system_details(target_region)
+    print('---------------------------')
+print('Done')
