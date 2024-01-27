d43195aee57fbbc1b64b87b9119e3d9e9cc5e7b9
Luiz Carlos K. Fukaya <luizcarloskazuyukifukaya@gmail.com>
2024-01-27 11:01:21 +0900

    PresignedURL with S3gV2 support

--- s3GetObjectPresignedURL.py
+++ s3GetObjectPresignedURL.py
@@ -5,6 +5,9 @@
 import requests    # To install: pip install requests
 from s3PresignedURLs import create_presigned_url
 
+# local expiration value (if not expecified, the default is defined in the s3PresignedURLs.py)
+EXPIRATION_VALUE=31536000 # seconds (365 days) (SigV2)
+
 # Target bucket and object (key) specified here
 # Define the target bucket and prefix
 target_bucket = "kfukaya-tagging"
@@ -12,10 +15,11 @@ target_bucket = "kfukaya-wasabi-doc"
 target_file = "data/test.txt" 
 target_file = "material.pptx" 
 
-print(target_bucket)
-print(target_file)
+print(f"Target bucket: {target_bucket}")
+print(f"Target file: {target_file}")
+print(f"Expiration: {EXPIRATION_VALUE} secs")
 
-url = create_presigned_url(target_bucket, target_file)
+url = create_presigned_url(target_bucket, target_file, EXPIRATION_VALUE)
 
 if url is not None:
     response = requests.get(url)
