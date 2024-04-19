2e6002715b26f1c997651613df370177e8040ba7
Luiz Carlos Kazuyuki Fukaya <luizcarloskazuyukifukaya@gmail.com>
2024-04-19 09:10:47 +0900

    Fix s3GetAccountTotalUtilization.py

--- s3GetAccountTotalUtilization.py
+++ s3GetAccountTotalUtilization.py
@@ -1,8 +1,13 @@
+# Get utilization of objects filtered by bucket name and prefix
+# This should help calculate the total storage within a "folder" 
+# or based on wild card "target_prefix*"
+
 import boto3
 import datetime
 
-JST = datetime.timezone(datetime.timedelta(hours=9), "JST")
-# https://qiita.com/nagataaaas/items/1582cebb51962f9a80e9
+DEFAULT_REGION = 'us-east-1' #ALWAYS
+TARGET_PROFILE = "wasabi"
+TARGET_REGION = "ap-northeast-1"
 
 def getS3Client(profileName: str) -> boto3.s3:
     # Use the following code to connect using Wasabi profile from .aws/credentials file
@@ -14,8 +19,7 @@ def getS3Client(profileName: str) -> boto3.s3:
     credentials = session.get_credentials()
     aws_access_key_id = credentials.access_key
     aws_secret_access_key = credentials.secret_key
-    DEFAULT_REGION = 'us-east-1' #ALWAYS
-    region = 'ap-northeast-1'
+    region = TARGET_REGION
     endpoint_url = 'https://s3.' + region + '.wasabisys.com'
     # endpoint_url = 'https://s3.wasabisys.com'
 
@@ -32,10 +36,17 @@ def getS3Client(profileName: str) -> boto3.s3:
 
     return s3Client
 
-def getS3ClientFromBucket(s3Client: boto3.client, bucket_name: str):
+def getS3ClientFromBucket(profileName: str, bucket_name: str):
 
     # Optin s3 client (set per each bucket)
     optin_s3 = None
+
+    # get s3 client and credentials
+    s3Client = getS3Client(profileName)
+    session = boto3.Session(profile_name=profileName)
+    credentials = session.get_credentials()
+    aws_access_key_id = credentials.access_key
+    aws_secret_access_key = credentials.secret_key
 
     location = s3Client.get_bucket_location(Bucket=bucket_name)
     
@@ -69,13 +80,33 @@ def convert_bytes(byte_size):
     
     return kb_size, mb_size, gb_size, tb_size
 
+def show_size(total_objects_size):
+    # get size per unit
+    kb, mb, gb, tb = convert_bytes(total_objects_size)
+
+    if tb > 1.0:
+        print(f"******** Account Total Consumed Size: {round(tb,4)} TB")
+        # print(f"******** Bucket Consumed Total Size: {tb} TB")
+    elif gb > 1.0:
+        print(f"******** Account Total Consumed Size: {round(gb,4)} GB")
+        # print(f"******** Bucket Consumed Total Size: {gb} GB")
+    elif mb > 1.0:
+        print(f"******** Account Total Consumed Size: {round(mb,4)} MB")
+        # print(f"******** Bucket Consumed Total Size: {mb} MB")
+    elif kb > 1.0:
+        print(f"******** Account Total Consumed Size: {round(kb,4)} KB")
+        # print(f"******** Bucket Consumed Total Size: {kb} KB")
+    print(f"******** Account Total Consumed Size: {total_objects_size} bytes")
+
+
 # main function
 def main():
     # Bucket Consumed object total size
     total_objects_size = 0
+    profile = TARGET_PROFILE
 
     # Get s3 client with the profile specified
-    s3 = getS3Client("wasabi")
+    s3 = getS3Client(profile)
     
     # List buckets
     response = s3.list_buckets()
@@ -87,7 +118,7 @@ def main():
         print(f'Bucket name: {bucket_name}') 
 
         # Get s3 client from the bucket (with optin supported)
-        optin_s3 = getS3ClientFromBucket(s3, bucket_name)
+        optin_s3 = getS3ClientFromBucket(profile, bucket_name)
         
         objects = optin_s3.list_objects_v2(Bucket=bucket["Name"])
         #print(objects['Name']) 
@@ -111,21 +142,8 @@ def main():
 
         print(f"... Bucket's Total Consumed Size: {total_objects_size} ")
 
-    kb,mb,gb,tb = convert_bytes(total_objects_size)
-    if tb > 1.0:
-        print(f"******** Account Total Consumed Size: {round(tb,4)} TB")
-        # print(f"******** Bucket Consumed Total Size: {tb} TB")
-    elif gb > 1.0:
-        print(f"******** Account Total Consumed Size: {round(gb,4)} GB")
-        # print(f"******** Bucket Consumed Total Size: {gb} GB")
-    elif mb > 1.0:
-        print(f"******** Account Total Consumed Size: {round(mb,4)} MB")
-        # print(f"******** Bucket Consumed Total Size: {mb} MB")
-    elif kb > 1.0:
-        print(f"******** Account Total Consumed Size: {round(kb,4)} KB")
-        # print(f"******** Bucket Consumed Total Size: {kb} KB")
-
-    print(f"******** Account Total Consumed Size: {total_objects_size} bytes")
+    # Show the storage size 
+    show_size(total_objects_size)
 
 # Executed when called directly
 if __name__ == "__main__":
